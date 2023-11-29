8706dde57fe5316a545d4cd61dd2354c7e1fbe6b
floppyhammer <tannhauser_chen@outlook.com>
2023-11-29 13:44:11 +0800

    Add signal callbacks

--- godot/main.gd
+++ godot/main.gd
@@ -1,12 +1,23 @@
 extends Node3D
 
-var lip_sync
+@onready var lip_sync = $LipSync
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	lip_sync = LipSync.new()
+	var audio_data: Array[float] = []
+	for i in 1024:
+		audio_data.append(randf())
+	
+	lip_sync.update(audio_data)
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	lip_sync.poll()
+
+
+func _on_lip_sync_panicked(error: String):
+	print(error)
+
+
+func _on_lip_sync_updated(output: Dictionary):
+	print(output)
